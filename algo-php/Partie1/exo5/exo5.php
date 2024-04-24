263b29202d2f188cdf1aaee94e9e389a108a25be
rivertroestler <167063264+rivertroestler@users.noreply.github.com>
2024-04-24 09:49:14 +0200

    exo5.php

--- algo-php/Partie1/exo5/exo5.php
+++ algo-php/Partie1/exo5/exo5.php
@@ -4,5 +4,11 @@
 Attention, la valeur générée devra être arrondie à 2 décimales.</p>
 
 <h2>Résultat</h2>
-
 <?php
+
+$valeurFrancs = 100;
+$valeurEuros = $valeurFrancs / 6.55957;
+//arrondir à deux décimales
+$ArrondiEuros = round($valeurEuros, 2);
+echo("Montant en francs: ".$valeurFrancs);
+echo("<br> 100 francs = $ArrondiEuros €");
